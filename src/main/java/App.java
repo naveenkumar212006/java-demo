e42158a94daa62e0ea10d93f5dd7467f89ca9296
naveenkumar212006 <naveenkumar21032006@gmail.com>
2026-07-12 11:54:03 +0530

    second commit

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("Hello, Jenkins + GitHub + Maven!");
+        System.out.println("CI/CD Pipeline is Working!");
+        System.out.println("=================================");
+    }
+}
